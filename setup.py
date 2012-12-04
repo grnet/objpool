e6edc69ef5d89c9a59849a75821970580a93b4f7
Dionysis Zindros <dionyziz@gmail.com>
2012-12-04 05:36:43 -0500

    Rename object pool library from pool to objpool

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='pool',
-    version='0.1.0',
+    name='objpool',
+    version='0.1.1',
     author='GRNet Development Team',
     author_email='synnefo@lists.grnet.gr',
-    packages=['pool'],
+    packages=['objpool'],
     scripts=[],
-    url='https://code.grnet.gr/projects/synnefo/repository/revisions/master/show/snf-common/synnefo/lib/pool',
+    url='https://github.com/dionyziz/objpool',
     license='LICENSE.txt',
-    description='A pooling library',
+    description='An object pooling library',
     long_description=open('README.txt').read(),
     install_requires=[],
 )
